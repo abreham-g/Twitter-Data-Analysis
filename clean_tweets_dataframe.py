696934ed7a9467e728697b942f367aa5386c0239
abreham <aynuyeabresh@gmail.com>
2021-06-25 16:28:03 -0300

    working on preproccesing

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -6,29 +8,34 @@ class Clean_Tweets:
         self.df = df
         print('Automation in Action...!!!')
         
-    def drop_unwanted_column(self, df:pd.DataFrame)->pd.DataFrame:
+    def drop_unwanted_column(self, df: pd.DataFrame, column: list) -> pd.DataFrame:
         """
         remove rows that has column names. This error originated from
         the data collection stage.  
         """
-        unwanted_rows = df[df['retweet_count'] == 'retweet_count' ].index
-        df.drop(unwanted_rows , inplace=True)
-        df = df[df['polarity'] != 'polarity']
+        try:
+            df.drop(columns=column, inplace=True)
+        except:
+            pass
         
         return df
+
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
         """
-        
-        df = df.drop_duplicates(inplace=True)
-        
-        return df
-    def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame
+        try:
+            df.drop_duplicates(inplace=True)
+        except:
+            pass
 
-        df['created_at'] = pd.to_datetime(df['Datetime'])
-        
-        df = df[df['created_at'] >= '2020-12-31' ]
+        return df
+
+    def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
+
+
+        #convert column to datetime
+        df['created_at'] = pd.to_datetime(df['created_at'])
         
         return df
     
@@ -52,7 +59,3 @@ class Clean_Tweets:
         
         df = df.drop(df[df['lang'] != 'en'].index)
         return df
-
-
-
-    remove_non_english_tweets = df = df.drop(df[df['lang'] != 'en'].index)
